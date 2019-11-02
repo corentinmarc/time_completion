560af91176ee414da51267b11332e80b8ff81b6b
Corentin Marc <corentin.marc@gmail.com>
2019-11-02 02:42:05 +0100

    ♻️Add Storybook centered addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,9 @@
 import { configure, addDecorator } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
+import centered from '@storybook/addon-centered/react';
 
 addDecorator(withKnobs);
+addDecorator(centered);
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../src', true, /\.stories\.tsx?$/), module);
